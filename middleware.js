c4a8c1349a23f0cf59136f2c37b9cd5ff9c21fb6
Abhishek Gupta <vivekgupta18278@gmail.com>
2025-07-22 23:37:35 +0530

    added features

--- middleware.js
+++ middleware.js
@@ -4,81 +4,96 @@ const { listingSchema, reviewSchema } = require("./schema");
 const ExpressError = require("./utils/expressError");
 const Review = require("./models/review.js");
 
-
-//check the user login or not 
-module.exports.isLoggedIn = (req,res,next)=>{
-    if(!req.isAuthenticated()){
-        // console.log(req)
-        req.session.redirectUrl = req.originalUrl;
-        const redirectRouteUrl =req.session.redirectUrl
-        // console.log(redirectRouteUrl)
-        req.flash('error',"Login the Page for perform this operation")
-        return res.redirect("/login")
-    }
-    next();
-}
+//check the user login or not
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    // console.log(req)
+    req.session.redirectUrl = req.originalUrl;
+    const redirectRouteUrl = req.session.redirectUrl;
+    // console.log(redirectRouteUrl)
+    req.flash("error", "Login the Page for perform this operation");
+    return res.redirect("/login");
+  }
+  next();
+};
+// for review
+module.exports.isLoggedInReview = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    const id = req.params.id || req.originalUrl.split("/")[2];
+    req.session.redirectUrl = `/listings/${id}`; // redirect to listing page
+    req.flash("error", "Please log in to post a review.");
+    return res.redirect("/login");
+  }
+  next();
+};
 // save the url which request was send
-module.exports.saveRedirectUrl = (req,res,next)=>{
-    if(req.session.redirectUrl){
-        res.locals.redirectUrl =req.session.redirectUrl ;
-    }
-    next();
-} 
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
 
 //checking the owner of the listing
-module.exports.isOwner = async(req,res,next)=>{
-    let { id } = req.params;
-    let listing = await Listing.findById(id);
-    if(!res.locals.currentUser || !listing.owner._id.equals(res.locals.currentUser._id)){
-        req.flash("error","You have not permission to Access this listing")
-        return res.redirect(`/listings/${id}`)
-    }
-    next()
-}
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (
+    !res.locals.currentUser ||
+    !listing.owner._id.equals(res.locals.currentUser._id)
+  ) {
+    req.flash("error", "You have not permission to Access this listing");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
 
-//check the author or owner of the review 
+//check the author or owner of the review
 module.exports.isReviewAuthor = async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    try {
-        const review = await Review.findById(reviewId);
+  const { id, reviewId } = req.params;
+  try {
+    const review = await Review.findById(reviewId);
 
-        if (!review) {
-            req.flash('error', 'Review not found!');
-            return res.redirect(`/listings/${id}`);
-        }
-
-        if (!res.locals.currentUser || !review.author.equals(res.locals.currentUser._id)) {
-            req.flash('error', 'You are not the author of this review.');
-            return res.redirect(`/listings/${id}`);
-        }
-
-        // Proceed if the user is the author of the review
-        return next();
-    } catch (err) {
-        req.flash('error', 'Something went wrong. Please try again.');
-        res.redirect(`/listings/${id}`);
+    if (!review) {
+      req.flash("error", "Review not found!");
+      return res.redirect(`/listings/${id}`);
     }
+
+    if (
+      !res.locals.currentUser ||
+      !review.author.equals(res.locals.currentUser._id)
+    ) {
+      req.flash("error", "You are not the author of this review.");
+      return res.redirect(`/listings/${id}`);
+    }
+
+    // Proceed if the user is the author of the review
+    return next();
+  } catch (err) {
+    req.flash("error", "Something went wrong. Please try again.");
+    res.redirect(`/listings/${id}`);
+  }
 };
 // for check the listing is validate or not
 module.exports.validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
 };
 
 // for check the review is validate or not
 
 module.exports.validateReview = (req, res, next) => {
-    let { error,value } = reviewSchema.validate(req.body);
-    if (error) {
-      let errMsg = error.details.map((el) => el.message).join(",");
-      throw new ExpressError(400, errMsg);
-    } else {
-        req.body = value;
-      next();
-    }
-  };
+  let { error, value } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    req.body = value;
+    next();
+  }
+};
--- router/review.js
+++ router/review.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router({mergeParams:true})
 const controllerReview = require('../controllers/reviews.js')
 const wrapasync = require('../utils/wrapasync.js')
-const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware.js')
+const { validateReview, isLoggedIn,isLoggedInReview, isReviewAuthor } = require('../middleware.js')
 
 //post the review 
-router.post('/',isLoggedIn, validateReview,wrapasync(controllerReview.createNewReview))
+router.post('/',isLoggedInReview, validateReview,wrapasync(controllerReview.createNewReview))
 //delete the review in the post
 router.delete('/:reviewId',isLoggedIn,isReviewAuthor,wrapasync(controllerReview.destroyReview))
 
